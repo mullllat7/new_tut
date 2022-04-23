af29829dacb437a6526f2391f3a28dfac907e959
mullllat <imsimpforkinder@gmail.com>
2022-04-23 17:34:42 +0600

    baya negr 2

--- tut_root/urls.py
+++ tut_root/urls.py
@@ -2,10 +2,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .yasg import urlpatterns as doc_urls
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.routers import DefaultRouter
+
+from app.course.views import CourseViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Authentication API',
+        default_version='v1',
+        description='Test Description',
+    ),
+    public=True
+)
+router = DefaultRouter()
+router.register('course', CourseViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    # path('admin/', admin.site.urls),
     path('account/', include('app.account.urls')),
     path('category/', include('app.category.urls')),
     path('course/', include('app.course.urls')),
@@ -14,4 +30,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-urlpatterns += doc_urls
+
